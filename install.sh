091ed7f1ea5dbf7834e4b6cca99716235d9d1b2f
ClanHost Australia <38305756+ClanHost@users.noreply.github.com>
2025-01-28 16:14:30 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo tailscale up
 sudo apt install ffmpeg -y
 sudo sed -i -e 's/GPSD_OPTIONS=""/GPSD_OPTIONS="\/dev\/ttyACM0"/g' /etc/default/gpsd
 #sudo gpsd /dev/serial0 -F /var/run/gpsd.sock
-curl -fsSL https://tailscale.com/install.sh | sh
+#curl -fsSL https://tailscale.com/install.sh | sh
 wget https://raw.githubusercontent.com/ClanHost/DashCam_Tracker/refs/heads/main/GPS.sh
 wget https://raw.githubusercontent.com/ClanHost/DashCam_Tracker/refs/heads/main/DASHCAM.sh
 chmod 777 GPS.sh
