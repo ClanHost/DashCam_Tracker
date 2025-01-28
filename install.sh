674a225c0359e3cee6fd9bd292ab20d28b5e22d2
ClanHost Australia <38305756+ClanHost@users.noreply.github.com>
2025-01-28 16:13:30 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,11 +12,12 @@ sudo sed -i -e 's/GPSD_OPTIONS=""/GPSD_OPTIONS="\/dev\/ttyACM0"/g' /etc/default/
 curl -fsSL https://tailscale.com/install.sh | sh
 wget https://raw.githubusercontent.com/ClanHost/DashCam_Tracker/refs/heads/main/GPS.sh
 wget https://raw.githubusercontent.com/ClanHost/DashCam_Tracker/refs/heads/main/DASHCAM.sh
+chmod 777 GPS.sh
+chmod 777 DASHCAM.sh
 
 
 sudo touch  /etc/systemd/system/GPS.service
 sudo touch  /etc/systemd/system/DASHCAM.service
-
 sudo echo "
 [Unit]
 Description = GPS
@@ -36,7 +37,6 @@ WantedBy = multi-user.target" > /etc/systemd/system/GPS.service
 
 sudo systemctl enable GPS
 sudo systemctl restart GPS
-
 sudo echo "
 [Unit]
 Description = DASHCAM
@@ -56,3 +56,5 @@ WantedBy = multi-user.target" > /etc/systemd/system/DASHCAM.service
 
 sudo systemctl enable DASHCAM
 sudo systemctl restart DASHCAM
+
+sudo reboot
