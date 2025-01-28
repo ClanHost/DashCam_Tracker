5bca7f3210a17d4c420ef98989444570fa1af7c1
ClanHost Australia <38305756+ClanHost@users.noreply.github.com>
2025-01-28 16:08:25 +1100

    Update GPS.sh

--- GPS.sh
+++ GPS.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-# FOR SOME STUPID REASON GPS WILL NOT UPDATE EVERY SINGLE TIME, THIS FIXED IT
-# https://forums.raspberrypi.com/viewtopic.php?t=331525&sid=3d088484b45e62242e02f4a2153af791&start=25
-ubxtool -p RESET
-
 while [ "$(fping google.com | grep alive)" != "google.com is alive" ]
     do
         sleep 3
 done
-
-    #DATA='{"token": "secret", "user": "secret", "message": "Car Movement"}'
+    #DATA='{"token": "qwerty", "user": "qwerty", "message": "Bike Movement"}'
     #curl -H 'Content-Type: application/json' -X POST https://api.pushover.net/1/messages.json -d "$DATA" > AAA.txt
 
 while true
@@ -23,8 +18,22 @@ while true
         WIFIS="$(sudo iwlist wlan0 scan | grep "ESSID" | base64 -w 0)"
         WIFIS="{\"WIFIS\": \"$WIFIS\"}"
 
+        # FOR SOME STUPID REASON GPS WILL NOT UPDATE EVERY SINGLE TIME, THIS FIXED IT
+        # https://forums.raspberrypi.com/viewtopic.php?t=331525&sid=3d088484b45e62242e02f4a2153af791&start=25
+        sudo ubxtool -p RESET
+
         #GPS LOCATION DETAILS
         GPS="$(gpspipe -w | grep -m 1 TPV | jq -c '{LAT: (.lat),LON: (.lon),SPD: (.speed),SATTIME: (.time),ALT: (.alt)}')"
+
+        SPEED="$(echo "$GPS" | jq -r '.SPD')"
+        echo "SPEED: $SPEED" > /home/pi/SPEED.dashcam
+
+        LOCATIONLON="$(echo "$GPS" | jq -r '.LON')"
+        LOCATIONLAT="$(echo "$GPS" | jq -r '.LAT')"
+        echo "$LON $LAT" > /home/pi/LOCATION.dashcam
+
+        DATE="$(date)"
+        echo "$DATE" > /home/pi/DATE.dashcam
 
         #APPEND JSON
         FINAL="$(jq --slurp 'add' <(echo "$BW") <(echo "$WIFIS") <(echo "$GPS"))"
@@ -35,6 +44,7 @@ while true
 
         #SEND FILE TO THE SERVER
         RESULT="$(curl --write-out '%{http_code}' --silent --output /dev/null --max-time 3 -X POST https://carpc.clanhost.com.au/API/index.php -H "Content-Type: application/json" -d "[{\"ACTION\": \"SETGPS\",\"GUID\": \"1234\", \"DATA\": [$FILE]}]")"
+
 
         #IF SUCCES THEN DELETE THE LOG FILE
         if [ $RESULT == 200 ]; then
